2d94fe88fb46b7c3b76da26d41465bca7c3069d2
Nabin Islam <sinabin62246@gmail.com>
2023-11-15 19:56:06 +0600

    server code transfered to root index file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,1 @@
-const app = require('./app');
-const { connectDB } = require('./config/db');
-const { port } = require('./secret');
 
-app.listen(port, async () => {
-  console.log(`Server is running at http://localhost:${port}`);
-  await connectDB();
-});
